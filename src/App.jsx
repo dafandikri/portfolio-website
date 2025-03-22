4c7e703f7c8f499388cc4f050e60441093810998
dafandikri <dafandikri@gmail.com>
2025-03-22 21:38:18 +0700

    feat: add background

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,39 @@ import Contact from './components/Contact'
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <ProfileCard />
-      
-      {/* Tech Stack and Skillsets side by side layout */}
-      <div id="tech-skills" className="section-sm">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-5">
-              <SkillsetsDialog />
+    <div className="windows95-app">
+      <div className="desktop-background">
+        <Navbar />
+        <div className="container py-4">
+          <div id="profile">
+            <ProfileCard />
+          </div>
+          
+          {/* Tech Stack and Skillsets side by side layout */}
+          <div id="tech-skills" className="section-sm">
+            <div className="row">
+              <div className="col-lg-5">
+                <SkillsetsDialog />
+              </div>
+              <div className="col-lg-7">
+                <TechStackDialog />
+              </div>
             </div>
-            <div className="col-lg-7">
-              <TechStackDialog />
-            </div>
+          </div>
+          
+          <div id="experience" className="mt-4">
+            <Experience />
+          </div>
+          
+          <div id="projects" className="mt-4">
+            <Project />
+          </div>
+          
+          <div id="contact" className="mt-4">
+            <Contact />
           </div>
         </div>
       </div>
-      
-      <Experience />
-      <Project />
-      <Contact />
     </div>
   )
 }
